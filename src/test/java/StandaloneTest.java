980a7ed4d298f89fdbebafaf8dcc3aa0e94b90fe
IzabelaMilisiewicz <izabela.milisiewicz@gmail.com>
2023-02-15 22:48:40 +0100

    Base test to build framework from (Rahul, section 17, videos 153-154)

--- src/test/java/StandaloneTest.java
+++ src/test/java/StandaloneTest.java
@@ -1,26 +1,32 @@
 import org.apache.commons.lang3.ObjectUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 import java.util.List;
 
 public class StandaloneTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://rahulshettyacademy.com/client");
+        driver.manage().window().maximize();
 
         String username = "izaecabs@gmail.com";
         String password = "Summer01";
+        String productName1 = "ZARA COAT 3";
+        String productName2 = "IPHONE 13 PRO";
 
         driver.findElement(By.id("userEmail")).sendKeys(username);
         driver.findElement(By.id("userPassword")).sendKeys(password);
@@ -32,8 +38,7 @@ public class StandaloneTest {
 
         //we have list of whole webelements, we can use with strems another locator which helps us too go deeper
         WebElement prod1 = products.stream().filter(product->product.findElement(By.cssSelector("b")).getText()
-                .equals("ZARA COAT 3")).findFirst().orElse(null);
-
+                .equals(productName1)).findFirst().orElse(null);
         //we can search now only in the scope of previous search so only in our matched webelement
         prod1.findElement(By.cssSelector(".card-body button:last-of-type")).click();
         //xpath - button[contains(text(),'Add To Cart')]
@@ -44,5 +49,20 @@ public class StandaloneTest {
 //        invisibilityOfElementLocated(By.cssSelector(".ng-animiting"))); -> this solutions was much slower than invisibilityOF
         driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
 
+        List <WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
+        Boolean match = cartProducts.stream().anyMatch(cartProduct -> cartProduct.getText().equalsIgnoreCase(productName1));
+        System.out.println(match);
+        Assert.assertTrue(match);
+        driver.findElement(By.cssSelector(".totalRow button")).click();
+
+        Actions a = new Actions(driver);
+        a.sendKeys(driver.findElement(By.xpath("//input[@placeholder='Select Country']")), "Pol").build().perform();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
+        driver.findElement(By.xpath("//span[contains(text(), 'Poland')]")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".action__submit")));
+        driver.findElement(By.cssSelector(".action__submit")).click();
+        String confirmMessage = driver.findElement(By.cssSelector(".hero-primary")).getText();
+        Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+        driver.close();
     }
 }
